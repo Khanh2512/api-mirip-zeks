8e38b6878e7147c277f25776fe0004a6f9ec0ebc
Hồ Viết Việt Khánh <79739158+Khanh2512@users.noreply.github.com>
2022-01-12 12:26:49 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -293,7 +293,7 @@ router.get('/cecan/malaysia', async (req, res, next) => {
         var result = data[Math.floor(Math.random() * data.length)];
         var buff = result.url
              res.json({
-             	author: 'Zeeone',
+             	author: 'Việt Khánh',
                  buff
              })
          })
@@ -616,7 +616,7 @@ router.get('/download/ytsearch', async (req, res, next) => {
         .then(data => {
         var result = data;
              res.json({
-             	author: 'Zeeone',
+             	author: 'Việt Khánh',
                  result
              })
          })
@@ -639,7 +639,7 @@ router.get('/download/ytmp4', async (req, res, next) => {
         .then(data => {
         var result = data;
              res.json({
-             	author: 'Zeeone',
+             	author: 'Việt Khánh',
                  result
              })
          })
@@ -663,7 +663,7 @@ router.get('/lk21/search', async (req, res, next) => {
         .then(data => {
         var result = data;
              res.json({
-             	author: 'Zeeone',
+             	author: 'Việt Khánh',
                  result
              })
          })
@@ -685,7 +685,7 @@ router.get('/lk21/terbaru', async (req, res, next) => {
         .then(data => {
         var result = data;
              res.json({
-             	author: 'Zeeone',
+             	author: 'Việt Khánh',
                  result
              })
          })
@@ -707,7 +707,7 @@ router.get('/lk21/comingsoon', async (req, res, next) => {
         .then(data => {
         var result = data;
              res.json({
-             	author: 'Zeeone',
+             	author: 'Việt Khánh',
                  result
              })
          })
@@ -729,7 +729,7 @@ router.get('/lk21/tvseries', async (req, res, next) => {
         .then(data => {
         var result = data;
              res.json({
-             	author: 'Zeeone',
+             	author: 'Việt Khánh',
                  result
              })
          })
@@ -752,7 +752,7 @@ router.get('/lk21/year', async (req, res, next) => {
         .then(data => {
         var result = data;
              res.json({
-             	author: 'Zeeone',
+             	author: 'Việt Khánh',
                  result
              })
          })
@@ -775,7 +775,7 @@ router.get('/lk21/country', async (req, res, next) => {
         .then(data => {
         var result = data;
              res.json({
-             	author: 'Zeeone',
+             	author: 'Việt Khánh',
                  result
              })
          })
@@ -798,7 +798,7 @@ router.get('/lk21/genre', async (req, res, next) => {
         .then(data => {
         var result = data;
              res.json({
-             	author: 'Zeeone',
+             	author: 'Việt Khánh',
                  result
              })
          })
@@ -823,7 +823,7 @@ router.get('/filmapik/search', async (req, res, next) => {
         .then(data => {
         var result = data;
              res.json({
-             	author: 'Zeeone',
+             	author: 'Việt Khánh',
                  result
              })
          })
@@ -846,7 +846,7 @@ router.get('/filmapik/kategori', async (req, res, next) => {
         .then(data => {
         var result = data;
              res.json({
-             	author: 'Zeeone',
+             	author: 'Việt Khánh',
                  result
              })
          })
@@ -869,7 +869,7 @@ router.get('/filmapik/play', async (req, res, next) => {
         .then(data => {
         var result = data;
              res.json({
-             	author: 'Zeeone',
+             	author: 'Việt Khánh',
                  result
              })
          })
@@ -891,7 +891,7 @@ router.get('/filmapik/terbaru', async (req, res, next) => {
         .then(data => {
         var result = data;
              res.json({
-             	author: 'Zeeone',
+             	author: 'Việt Khánh',
                  result
              })
          })
@@ -916,7 +916,7 @@ router.get('/news/cnn', async (req, res, next) => {
         .then(data => {
         var result = data;
              res.json({
-             	author: 'Zeeone',
+             	author: 'Việt Khánh',
                  result
              })
          })
@@ -939,7 +939,7 @@ router.get('/news/cnbc', async (req, res, next) => {
         .then(data => {
         var result = data;
              res.json({
-             	author: 'Zeeone',
+             	author: 'Việt Khánh',
                  result
              })
          })
@@ -962,7 +962,7 @@ router.get('/news/republika', async (req, res, next) => {
         .then(data => {
         var result = data;
              res.json({
-             	author: 'Zeeone',
+             	author: 'Việt Khánh',
                  result
              })
          })
@@ -985,7 +985,7 @@ router.get('/news/tempo', async (req, res, next) => {
         .then(data => {
         var result = data;
              res.json({
-             	author: 'Zeeone',
+             	author: 'Việt Khánh',
                  result
              })
          })
@@ -1008,7 +1008,7 @@ router.get('/news/antara', async (req, res, next) => {
         .then(data => {
         var result = data;
              res.json({
-             	author: 'Zeeone',
+             	author: 'Việt Khánh',
                  result
              })
          })
@@ -1030,7 +1030,7 @@ router.get('/news/kumparan', async (req, res, next) => {
         .then(data => {
         var result = data;
              res.json({
-             	author: 'Zeeone',
+             	author: 'Việt Khánh',
                  result
              })
          })
@@ -2845,7 +2845,7 @@ router.get('/other/ghstalk', async (req, res, next) => {
         .then(data => {
         var result = data;
              res.json({
-             	author: 'Zeeone',
+             	author: 'Việt Khánh',
                  result
              })
          })
@@ -2868,7 +2868,7 @@ router.get('/other/repostalk', async (req, res, next) => {
         .then(data => {
         var result = data;
              res.json({
-             	author: 'Zeeone',
+             	author: 'Việt Khánh',
                  result
              })
          })
